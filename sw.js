bb12f6e41020ee05f547a1651b62d340bc6c28e4
marceoviedo1980 <marceoviedo1980@gmail.com>
2026-04-07 20:41:50 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'farmacia-v1';
+const FILES = ['./index.html', './medicamentos.json', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
